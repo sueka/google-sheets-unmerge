20eb92ca8242eb27afb5906ca40142dcb784eb03
sueka <mail@sueka.me>
2023-10-15 12:10:22 +0900

    :white_check_mark: Add stubs

--- src/SpreadsheetAppStub.ts
+++ src/SpreadsheetAppStub.ts
@@ -1,8 +1,65 @@
-const rangeA3C3Stub: IRange = {
+const rangeA3C3Stub: IRange = { // includes no merged cells.
   getValues() {
     return [
       ['coo', 'buzz', 'neigh'],
     ]
+  },
+
+  getMergedRanges() {
+    return []
+  },
+}
+
+const rangeA1C3Stub: IRange = { // includes the entire merged cell.
+  getValues() {
+    return [
+      ['BIG CELL', '', 'meow'],
+      ['', '', 'croak'],
+      ['coo', 'buzz', 'neigh'],
+    ]
+  },
+
+  getMergedRanges() {
+    const mergedRange = {
+      getValues() {
+        return [
+          ['BIG CELL', ''],
+          ['', ''],
+        ]
+      },
+
+      getMergedRanges() {
+        return [mergedRange]
+      },
+    }
+
+    return [mergedRange]
+  },
+}
+
+const rangeB2C3Stub: IRange = { // includes some of the merged cell.
+  getValues() {
+    return [
+      ['', 'croak'],
+      ['buzz', 'neigh'],
+    ]
+  },
+
+  getMergedRanges() {
+    const mergedRange = {
+      getValues() {
+        return [
+          ['BIG CELL', ''],
+          ['', ''],
+        ]
+      },
+
+      getMergedRanges() {
+        return [mergedRange]
+      },
+    }
+
+    return [mergedRange]
   },
 }
 
@@ -10,6 +67,8 @@ const sheetStub: ISheet = {
   getRange(a1Notation) {
     switch (a1Notation) {
       case 'A3:C3': return rangeA3C3Stub
+      case 'A1:C3': return rangeA1C3Stub
+      case 'B2:C3': return rangeB2C3Stub
       default: throw new Error // unreachable
     }
   },
--- src/types.ts
+++ src/types.ts
@@ -8,4 +8,5 @@ interface ISheet {
 
 interface IRange {
   getValues(): string[][]
+  getMergedRanges(): IRange[]
 }
