2399524daba6fb476d3c5dffad2f5d127c017b27
sueka <mail@sueka.me>
2023-10-16 20:19:51 +0900

    :white_check_mark: Implement unmerge()

--- src/Unmerger.ts
+++ src/Unmerger.ts
@@ -7,7 +7,30 @@ export default class Unmerger {
 
   run(a1Notation: string): any[][] {
     const range = this.#spreadsheetApp.getActiveSheet().getRange(a1Notation)
+    const values = range.getValues()
+    const offsetRows = range.getRow() - 1
+    const offsetColumns = range.getColumn() - 1
+    const mergedRanges = range.getMergedRanges()
 
-    return range.getValues()
+    for (const mergedRange of mergedRanges) {
+      const value = mergedRange.getValue()
+      const top = mergedRange.getRow()
+      const bottom = mergedRange.getLastRow()
+      const left = mergedRange.getColumn()
+      const right = mergedRange.getLastColumn()
+
+      for (let i = top - offsetRows - 1; i <= bottom - offsetRows - 1; i++) {
+        for (let j = left - offsetColumns - 1; j <= right - offsetColumns - 1; j++) {
+          if (i < 0 || j < 0) {
+            continue
+          }
+
+          // @ts-expect-error TS2532
+          values[i][j] = value
+        }
+      }
+    }
+
+    return values
   }
 }
